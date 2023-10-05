91b0a579be57a48ebc05885ccf1c2479166f2359
SteDor <136347112+SteDor@users.noreply.github.com>
2023-10-05 14:59:39 +0200

    Add files via upload
    update 15:00

--- Database.php
+++ Database.php
@@ -1,10 +1,13 @@
 <?php 
 class Database {
   
-    function __construct(public $servername, public $username, public $password, public $databasename){
-        
+    function __construct(public $servername, public $username, public $password, public $databasename){}
 
-    }
+    public function checkConnection($conn) {
+      if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+        }
+  }    
 
     // ceate new database if user exist in PhpMyAdmin 
     // u can name your database with this function
@@ -12,9 +15,7 @@ class Database {
         // Create connection
         $conn = new mysqli($servername, $username, $password);
         // Check connection
-        if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
-        }
+        $this->checkConnection($conn);
         // Create database 
         $sql = "CREATE DATABASE $databasename";
         if ($conn->query($sql) === TRUE) {
@@ -24,48 +25,47 @@ class Database {
         }
         $conn->close();    
     }
+
+    // create table with primary key and columns
+    function createTable($servername, $username, $password, $databasename, $tablename, $sqlInfoString){
+      $this->createTableWithKey($servername, $username, $password, $databasename, $tablename);
+      $this->addColumns($servername, $username, $password, $databasename, $tablename, 'ID', $sqlInfoString);
+    }
+
+
     // create Table with Name and create column ID (Primary Key)
-    public function createTable($servername, $username, $password, $databasename, $tablename ){
+    public function createTableWithKey($servername, $username, $password, $databasename, $tablename ){
       // Create connection
       $conn = mysqli_connect($servername, $username, $password, $databasename);
       // Check connection
-      if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-      }
+      $this->checkConnection($conn);
       // sql to create table
       $sql = "CREATE TABLE " .  $tablename . "(
         ID int NOT NULL,
         PRIMARY KEY (ID)
       
       )";
-
       if (mysqli_query($conn, $sql)) {
-        echo ("Table " .  $tablename .  " created successfully");
+        echo ("Table " .  $tablename .  " created successfully<br>");
       } else {
         echo ("Error creating table: " . $tablename . "") . mysqli_error($conn);
       }
-
       mysqli_close($conn);
     }
 
-    // Add columns to an existing table in the DB
-    public function addColumns($servername, $username, $password, $databasename, $tablename, $sqlInfoString){
+    // Add column to an existing table in the DB
+    public function addColumn($servername, $username, $password, $databasename, $tablename, $sqlInfoString){
       $conn = mysqli_connect($servername, $username, $password, $databasename);
       // make $sqlInfoString to Array for sql statement
       $infoArray = explode('|', $sqlInfoString);
-
       // Check connection
-      if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-      }
-      // sql to create table
+      $this->checkConnection($conn);
+      // sql to create column
       $sql = ('ALTER TABLE ' . $tablename . 
       ' ADD ' . $infoArray[0] . 
       ' ' . $infoArray[2] . ' NOT NULL AFTER ' 
       . $infoArray[1]); 
-      
-      
-
+      // check if created
       if (mysqli_query($conn, $sql)) {
         echo ('Adedd column '. $infoArray[0] . ' to table ' . $tablename . ' after column ' .  $infoArray[1]);
       } else {
@@ -76,55 +76,41 @@ class Database {
 
     }
 
-    // Add column to an existing table in the DB
-    public function addColumn($servername, $username, $password, $databasename, $tablename, $addColumn, $sqlInfoString){
+    // Add columns to an existing table in the DB next to a column do secify
+    public function addColumns($servername, $username, $password, $databasename, $tablename, $addColumn, $sqlInfoString){
       $conn = mysqli_connect($servername, $username, $password, $databasename);
-      // make $infoArray for  Sql Statement
-      
+      // make $infoArray for  Sql Statement      
       $infoArray = explode('|',$sqlInfoString);
 
-      echo ('add to: ' . $addColumn);
-
       // for testing
-      for ($i = 0; $i < count($infoArray); $i++)
-        {
-          echo($infoArray[$i].", ");
-        }
-        echo("</br>");
-        echo('size = ' . count($infoArray). '<br>');
+      // for ($i = 0; $i < count($infoArray); $i++)
+      //   {
+      //     echo($infoArray[$i].", ");
+      //   }
+      //   echo("</br>");
+      //   echo('size = ' . count($infoArray). '<br>');
 
-      if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-      }
+        $this->checkConnection($conn);
+
       // sql to create multiple columns
       for ($count = 0; $count < count($infoArray); $count+=2){
-        echo('count = ' . $count . '<br>');
-
         $sql = ('ALTER TABLE ' . $tablename         //name of table
         . ' ADD ' . $infoArray[$count] . ' '                //name of column
         . $infoArray[$count + 1] . ' NOT NULL AFTER '   //type
         . $addColumn);                  //column to add
-      
+        // to check if created
         if (mysqli_query($conn, $sql)) {
           echo ('Adedd column '. $infoArray[$count] . ' to table ' 
           . $tablename . ' after column ' .  $addColumn . '<br>');
         } else {
           echo ('No Columns added') . mysqli_error($conn);
         }
-        
-        
-        
-        
-        //echo ('Adedd column '. $infoArray[$count] . ' to table ' . $tablename . ' after column ' .  $addColumn . '<br>');
-      
-
-      $addColumn = $infoArray[$count];
-      
+        // set add Column to name of last insert column
+        $addColumn = $infoArray[$count];
       }
 
       mysqli_close($conn);
     }  
     
-
 }
 ?>
--- test.php
+++ test.php
@@ -4,9 +4,10 @@
     $servername = 'localhost';
     $username = "Sepax";
     $password = '123';
-    $databasename = 'PartyDB';
+    $databasename = 'TestDB';
 
     $db = new Database($servername , $username, $password, $databasename);
+    
 
     echo ('Server: ' . $servername . '<br>User: ' . $username . '<br> PW: ' 
         . $password . '<br>Database Name: ' . $databasename . '<br>');
@@ -17,12 +18,16 @@
 
 
     //create DB test
-        // $db->createDatabase($servername, $username, $password, $databasename);
+    // $db->createDatabase($servername, $username, $password, $databasename);
     //create Table
     // Tabelname has to be one Word without space 
-    // $db->createTable($servername , $username, $password, $databasename, 'Guest_List'); 
+    //$db->createTableWithKey($servername , $username, $password, $databasename, 'Test2'); 
     // add columns with name of table and anarray for the SQL statement(Name of table | Name of column where to add new column | Type of column)
     // $db-> addColumn($servername , $username, $password, $databasename, 'Guest_List', 'Kurt|ID|INT');
 
-    $db-> addColumn($servername , $username, $password, $databasename, 'Guest_List', 'ID', 'Preis|INT|String|VARCHAR(60)');
+    //$db-> addColumns($servername , $username, $password, $databasename, 'Test2', 'ID', 'Kilo|INT|Nomen|VARCHAR(60)|Text|TEXT');
+
+    $db->createTable($servername, $username, $password, $databasename, 'Test2', 'Kilo|INT|Nomen|VARCHAR(60)|Text|TEXT');
+
+
 ?>
